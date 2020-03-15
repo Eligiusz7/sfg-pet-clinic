2a3d65a64874a709a51e6cf0cdb6227534a024a0
Eligiusz7 <45896628+Eligiusz7@users.noreply.github.com>
2020-03-15 15:01:29 +0100

    Added PetTypes to bootstrap. Closes #32

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/PetTypeMapService.java
@@ -2,9 +2,11 @@ package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.PetType;
 import com.example.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
