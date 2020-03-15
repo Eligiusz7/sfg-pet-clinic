faac00c5594adad732a30c55430a8c4a376a5b89
Eligiusz7 <45896628+Eligiusz7@users.noreply.github.com>
2020-03-15 20:56:43 +0100

    Added specialties to vets on startup. Closes #34.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialityMapService.java
@@ -1,11 +1,13 @@
 package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.Speciality;
-import com.example.sfgpetclinic.services.SpecialtiesService;
+import com.example.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
     @Override
     public Set<Speciality> findAll() {
         return super.findAll();
